26d29bf42b59958c98e0ae8d31e0869d3ad81a35
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2024-04-23 15:47:39 +0200

    Add project files.

---
+++ WebApplication1/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using WebApplication1.Models;
+using WebApplication1.Models.Dto;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase 
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>
+            {
+                new VillaDTO{Id=1, Name="Pool View"},
+                new VillaDTO{Id=2, Name="Beach View"},
+            };
+        }
+    }
+}
